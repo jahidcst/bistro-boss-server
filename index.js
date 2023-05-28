3ce20a7e7e935155482b27d6b460e0488d68b9f8
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-05-28 20:05:14 +0600

    something

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 8000;
 
@@ -10,8 +11,6 @@ app.use(cors());
 app.use(express.json())
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ncm3v5o.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,14 +29,32 @@ async function run() {
 
     const menuCollection = client.db("bistroDb").collection("menu")
     const reviewCollection = client.db("bistroDb").collection("reviews")
+    const cartCollection = client.db("bistroDb").collection("carts")
 
-    app.get('/menu', async(req, res) =>{
-        const result = await menuCollection.find().toArray()
-        res.send(result)
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray()
+      res.send(result)
     })
-    app.get('/reviews', async(req, res) =>{
-        const result = await reviewCollection.find().toArray()
-        res.send(result)
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewCollection.find().toArray()
+      res.send(result)
+    })
+
+    // cart collection------------
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([])
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
@@ -50,12 +67,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) =>{
-    res.send('boss is runnig')
+app.get('/', (req, res) => {
+  res.send('boss is runnig')
 })
 
-app.listen(port, () =>{
-    console.log(`Bistro boss is sitting on port ${port}`);
+app.listen(port, () => {
+  console.log(`Bistro boss is sitting on port ${port}`);
 })
